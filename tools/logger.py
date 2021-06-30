1c143ecf745940f123a8ccc8091f2502d7261c2e
sergengoube <Came1roun*>
2021-06-30 19:00:00 +0200

    Initial commit

--- tools/logger.py
+++ tools/logger.py
@@ -11,7 +11,7 @@ logging.basicConfig(level=logging.DEBUG,
                     )
 # define a Handler which writes INFO messages or higher to the sys.stderr
 console = logging.StreamHandler()
-filehandler = RotatingFileHandler(filename=os.path.join("..", "logs", "ic_log.log"), maxBytes=2000000, backupCount=20)
+filehandler = RotatingFileHandler(filename=os.path.join("logs", "ic_log.log"), maxBytes=2000000, backupCount=20)
 console.setLevel(logging.INFO)
 # set a format which is simpler for console use
 formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')

5b8bdf4a875f3ba8e1b53f0b4f5339900a69d9e6
sergengoube <Came1roun*>
2022-04-01 20:24:01 +0200

    Initial commit

--- application.py
+++ application.py
@@ -309,5 +309,9 @@ def get_stock_prices():
 
 
 if __name__ == '__main__':
+    #/etc/letsencrypt/live/stocks.investingclub.io/
+    # /etc/letsencrypt/live/stocks.investingclub.io/privkey.pem
     rt = RepeatedTimer(1200, get_stock_prices)
-    socket_.run(app, debug=True, host='0.0.0.0', port=5005)
+    socket_.run(app, debug=True, host='0.0.0.0', port=5005,
+                keyfile='/etc/letsencrypt/live/stocks.investingclub.io/',
+                certfile='/etc/letsencrypt/live/stocks.investingclub.io/privkey.pem')
